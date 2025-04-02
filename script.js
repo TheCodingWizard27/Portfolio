2835f5ed35cb3324a7e0648b56a1c7740eea082a
Raunak Upreti <raunakupretics26@gmail.com>
2025-04-01 20:33:03 -0500

    Event Listener added

---
+++ script.js
@@ -0,0 +1,9 @@
+// Event Listener Initialization
+document.addEventListener("DOMContentLoaded", () => {
+  initMobileMenu();
+  initTypingEffect();
+  initScrollEvents();
+  initThemeToggle();
+  initSmoothScrolling();
+  initAnimations();
+});
